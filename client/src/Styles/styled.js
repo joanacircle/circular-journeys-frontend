66c23480af823266ca50836ce93f48f5f7d2f182
Circle <ja95175@gmail.com>
2023-01-17 14:32:42 +0800

    change the padding of Container

--- client/src/Styles/styled.js
+++ client/src/Styles/styled.js
@@ -31,7 +31,7 @@ export const Text = styled.p`
 // flex box
 export const Container = styled.div`
   max-width:1920px;
-  margin:0 auto;
+  padding:0 380px;
   width:100%;
   background:${props => props.background || Theme.White};
 `

c7a811d5307668f28b8b4be7eaf1f4af03285dae
chenchiaho <chenchiaho@gmail.com>
2023-01-21 06:11:59 +0800

    add style for header

--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -5,13 +5,13 @@ import Footer from './Footer'
 const MainLayout = () => {
   return (
     <>
-      <div className="col-lg-8 mx-auto p-4 py-md-5">
+      <>
         <Header />
         <main>
           <Outlet />
         </main>
         <Footer />
-      </div>
+      </>
     </>
   )
 }

cc9e63bad8cb44dfaa0562e6498b1b827e3a3942
chenchiaho <chenchiaho@gmail.com>
2023-01-25 03:37:23 +0800

    edit style.scss, add cards and filter

--- client/src/pages/Shop/components/ProductList/ProductItem.js
+++ client/src/pages/Shop/components/ProductList/ProductItem.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import './ProductItem.scss'
 
 const ProductItem = (props) => {
   const { id, name, picture, price, tags } = props.product
-  return (
-    <>
 
-    </>
+  return (
+    <div className="product-card">
+
+      <img src={require(`images/shop/products/${picture}`)} />
+      <h3>{name}</h3>
+      <p>${price}</p>
+
+    </div>
   )
 }
 

d80c755bafa0bd16cfcfeca0d9564766d072c85e
chenchiaho <chenchiaho@gmail.com>
2023-01-29 21:21:58 +0800

    stripe second commit

--- client/src/components/Stripe/StripeContainer.js
+++ client/src/components/Stripe/StripeContainer.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-import { PaymentForm } from './PaymentForm'
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+import React from "react"
+import PaymentForm from "./PaymentForm"
 
+const PUBLIC_KEY = 'pk_test_51MVV2XL9vOMbHljwYxDpUN8irGCzLHgUXSxQamceHfPCY7BdMKVaNLkzubYd2kKozQGY5Lh3EVPp6LYcrmVNy4ft00KqpV0vfV'
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
 export const StripeContainer = () => {
 
-  const PUBLIC_KEY = 'pk_test_51MVV2XL9vOMbHljwYxDpUN8irGCzLHgUXSxQamceHfPCY7BdMKVaNLkzubYd2kKozQGY5Lh3EVPp6LYcrmVNy4ft00KqpV0vfV'
-
-  const stripeTestPromise = loadStripe(PUBLIC_KEY)
-
   return (
-    <Elements stripe={stripeTestPromise} >
+    <Elements stripe={stripeTestPromise}>
       <PaymentForm />
     </Elements>
   )
 }
+
+
+
+// import { Elements } from "@stripe/react-stripe-js"
+// import { loadStripe } from "@stripe/stripe-js"
+// import React from "react"
+// import PaymentForm from "./PaymentForm"
+
+
+// const PUBLIC_KEY = 'pk_test_51MVV2XL9vOMbHljwYxDpUN8irGCzLHgUXSxQamceHfPCY7BdMKVaNLkzubYd2kKozQGY5Lh3EVPp6LYcrmVNy4ft00KqpV0vfV'
+
+// const stripeTestPromise = loadStripe(PUBLIC_KEY)
+
+// const StripeContainer = () => {
+//   return (
+//     <Elements stripe={stripeTestPromise}>
+//       <PaymentForm />
+//     </Elements>
+//   )
+// }
+// export default StripeContainer

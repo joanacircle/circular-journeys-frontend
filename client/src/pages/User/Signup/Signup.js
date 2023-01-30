d0d37454470c3a5de319f8bd43df339677915b6d
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-01-31 00:06:28 +0800

    create signup api

--- client/src/pages/User/Signup/Signup.js
+++ client/src/pages/User/Signup/Signup.js
@@ -1,19 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import validator from 'validator'
 import './Signup.scss'
 
+const url = 'http://localhost:3002/api/user/signup'
+
 const Signup = () => {
+  const [inputValue, setInputValue] = useState({})
+  // const [error, setError] = useState({})
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setInputValue({ ...inputValue, [name]: value })
+  }
+
+  // const validate = () => {
+  //   const newErrors = {}
+  //   if (!validator.isEmail(inputValue.userEmail)) {
+  //     newErrors.email = '輸入無效的信箱，請確認！'
+  //   }
+  //   if (!validate.isMobilePhone(inputValue.userTelephone, 'zh-TW')) {
+  //     newErrors.telephone = '輸入無效的電話！'
+  //   }
+  //   setError(newErrors)
+  //   return Object.keys(newErrors).length === 0
+  // }
+
+  const isObjectEmpty = (obj) => {
+    for (const key in obj) {
+      if (!obj[key]) return false
+    }
+    return true
+  }
+
+  const handleSubmitButton = () => {
+    if (isObjectEmpty(inputValue)) return
+    axios.post(url, inputValue)
+      .then(res => {
+        const data = res.data
+        console.log(data)
+        if (data.state) {
+          alert(data.message)
+          location.href = '/'
+        } else {
+          alert(data.message)
+          location.href = '/signup'
+        }
+      })
+    setInputValue({})
+  }
+
   return (
     <div className="signup-place">
       <div className="col">
         <h1>Signup</h1>
-        <form>
+        <form method="post">
           <div>
             <label htmlFor="userFirstName">姓：</label>
             <input
               type="text"
               id="userFirstName"
               name="userFirstName"
+              onChange={handleInputChange}
               required
             />
             <label htmlFor="userLastName">名：</label>
@@ -21,6 +70,7 @@ const Signup = () => {
               type="text"
               id="userLastName"
               name="userLastName"
+              onChange={handleInputChange}
               required
             />
           </div>
@@ -30,6 +80,8 @@ const Signup = () => {
               type="email"
               id="userEmail"
               name="userEmail"
+              // value={error.email && error.email}
+              onChange={handleInputChange}
               placeholder='example@email.com'
               required
             />
@@ -40,17 +92,29 @@ const Signup = () => {
               type="password"
               id="userPassword"
               name="userPassword"
+              onChange={handleInputChange}
               required
             />
           </div>
           <div>
             <label htmlFor="userSex">性別：</label>
-            <select name="userSex" id="userSex" required>
+            <select name="userSex" id="userSex" onChange={handleInputChange} required>
               <option>請選擇</option>
-              <option value="male">男</option>
-              <option value="female">女</option>
-              <option value="other">其他</option>
+              <option value="m">男</option>
+              <option value="f">女</option>
+              <option value="o">其他</option>
             </select>
+          </div>
+          <div>
+            <label htmlFor="userTelephone">電話：</label>
+            <input
+              type="text"
+              id="userTelephone"
+              name="userTelephone"
+              // value={error.telephone && error.telephone}
+              onChange={handleInputChange}
+              required
+            />
           </div>
           <div>
             <label htmlFor="userBirthday">生日：</label>
@@ -58,6 +122,7 @@ const Signup = () => {
               type="date"
               id="userBirthday"
               name="userBirthday"
+              onChange={handleInputChange}
               required
             />
           </div>
@@ -67,6 +132,7 @@ const Signup = () => {
               type="text"
               id="userCountry"
               name="userCountry"
+              onChange={handleInputChange}
               required
             />
           </div>
@@ -76,6 +142,7 @@ const Signup = () => {
               type="text"
               id="userCity"
               name="userCity"
+              onChange={handleInputChange}
               required
             />
           </div>
@@ -85,11 +152,17 @@ const Signup = () => {
               type="text"
               id="userAddress"
               name="userAddress"
+              onChange={handleInputChange}
               required
             />
           </div>
           <div className="btn-place">
-            <input className="btn submit-btn" type="submit" value='註冊' />
+            <input
+              className="btn submit-btn"
+              type="submit"
+              value='註冊'
+              onClick={handleSubmitButton}
+            />
             <Link to="/login">
               <input className="btn backto-btn" type="button" value='返回登入' />
             </Link>

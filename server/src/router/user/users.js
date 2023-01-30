3c93d800eeb3c96dcc767b5d189d83ef2d985142
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-01-30 16:55:03 +0800

    create connect-sql and user router

--- server/src/router/user/users.js
+++ server/src/router/user/users.js
@@ -1,2 +1,16 @@
 const express = require('express')
+const db = require('../../modules/connect-sql')
+
 const router = express.Router()
+
+
+//http://localhost:3002/api/user/userslist
+router.get('/userslist',(req,res,next) => {
+  const sql = 'SELECT * FROM `users_information` ORDER BY id ASC'
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    res.json(result)
+  })
+})
+
+module.exports = router

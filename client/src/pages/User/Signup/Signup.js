7d716d8293bd078b57cc32afa5daf18e9ab06568
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-01-30 09:47:02 +0800

    merge to main

--- client/src/pages/User/Signup/Signup.js
+++ client/src/pages/User/Signup/Signup.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Signup.scss'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="signup-place">
+      <div className="col">
+        <h1>Signup</h1>
+        <form>
+
+        </form>
+      </div>
+    </div>
   )
 }
 

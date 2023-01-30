ba6bbb2b237fc4cdd3d858e7fcd71bb57722558c
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-01-30 16:55:03 +0800

    create connect-sql and user router

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 const middleware = require('./middleware')
+const user = require('./router/user/users')
+
 require('dotenv').config()
 
 app.use(middleware)
-
-app.get('/', (req, res) => {
-  res.send('Server!')
-})
+app.use('/api/user',user)
 
 module.exports = app
---
+++ server/src/modules/connect-sql.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2')
+require('dotenv').config()
+
+const user = process.env.DATABASEUSER_DEV
+const password = process.env.DATABASEPASSWORD_DEV
+const port = process.env.DATABASE_PORT_DEV
+const database = process.env.DATABASE_DEV
+
+const pool = mysql.createPool({
+  host:'localhost',
+  user,
+  password,
+  port,
+  database,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0
+})
+
+module.exports = pool

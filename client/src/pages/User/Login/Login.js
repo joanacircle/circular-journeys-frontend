e4dcc9cd025a887e9389e5e37506f8f57e5e0fe7
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-01-31 15:59:10 +0800

    fix bug and create member centre file

--- client/src/pages/User/Login/Login.js
+++ client/src/pages/User/Login/Login.js
@@ -10,70 +10,72 @@ import { AiFillApple } from 'react-icons/ai'
 const Login = () => {
   return (
     <Container className="background">
-      <Row className="background-img" justify={'center'}>
-        <Col className='login-place'>
-          <form>
-            <div className='login-flex'>
-              <LoginTitle size={Theme.H1} color={Theme.Orange}>Login</LoginTitle>
-              <Input
-                type='text'
-                placeholder='Email'
-              />
-              <Input
-                type='password'
-                placeholder='Password'
-              />
-              <div className='login-remember-place'>
-                <div className='login-checkbox'>
-                  <input
-                    type='checkbox'
-                    name='remember'
-                    id='remember'
-                  />
-                  <label htmlFor='remember'>
-                    記住我
-                  </label>
+      <div className="background-rgba">
+        <Row className="background-img" justify={'center'}>
+          <Col className='login-place'>
+            <form>
+              <div className='login-flex'>
+                <LoginTitle color={Theme.White}>Login</LoginTitle>
+                <Input
+                  type='text'
+                  placeholder='Email'
+                />
+                <Input
+                  type='password'
+                  placeholder='Password'
+                />
+                <div className='login-remember-place'>
+                  <div className='login-checkbox'>
+                    <input
+                      type='checkbox'
+                      name='remember'
+                      id='remember'
+                    />
+                    <label className='text-size' htmlFor='remember'>
+                      記住我
+                    </label>
+                  </div>
+                  <div>
+                    <Link className='text-size' to='/signup'>加入會員？</Link>
+                    <a className='text-size' href="#">忘記密碼？</a>
+                  </div>
                 </div>
+                <Input
+                  className='input-submit'
+                  type='submit'
+                  value='登入'
+                />
                 <div>
-                  <Link to='/signup'>加入會員？</Link>
-                  <a href="#">忘記密碼？</a>
+                  <p className='text-size'>or enter with</p>
+                </div>
+                <div className='login-contact-link'>
+                  <a className='btn input-submit' href="http:#">
+                    <button
+                      type="button"
+                    >
+                      <FcGoogle size={25} />
+                    </button>
+                  </a>
+                  <a className='btn btn-fb input-submit' href="http:#">
+                    <button
+                      type="button"
+                    >
+                      <FaFacebookSquare color='#fff' size={25} />
+                    </button>
+                  </a>
+                  <a className='btn btn-ap input-submit' href="http:#">
+                    <button
+                      type="button"
+                    >
+                      <AiFillApple color='#fff' size={25} />
+                    </button>
+                  </a>
                 </div>
               </div>
-              <Input
-                className='input-submit'
-                type='submit'
-                value='登入'
-              />
-              <div>
-                <p>or enter with</p>
-              </div>
-              <div className='login-contact-link'>
-                <a className='btn input-submit' href="http:#">
-                  <button
-                    type="button"
-                  >
-                    <FcGoogle size={25} />
-                  </button>
-                </a>
-                <a className='btn btn-fb input-submit' href="http:#">
-                  <button
-                    type="button"
-                  >
-                    <FaFacebookSquare color='#fff' size={25} />
-                  </button>
-                </a>
-                <a className='btn btn-ap input-submit' href="http:#">
-                  <button
-                    type="button"
-                  >
-                    <AiFillApple color='#fff' size={25} />
-                  </button>
-                </a>
-              </div>
-            </div>
-          </form>
-        </Col>
-      </Row>
+            </form>
+          </Col>
+        </Row>
+      </div>
     </Container>
   )
 }

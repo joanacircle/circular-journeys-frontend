3d1177fc77be6bff76232ed76ee8975a3ab04053
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-02-02 23:21:56 +0800

    add forgot modal

---
+++ client/src/pages/User/Login/Forgot.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import './Forgot.scss'
+import { BiShow, BiHide } from 'react-icons/bi'
+
+const Forgot = ({ showPassword, handleShowPasswordButton }) => {
+  const [showVerifyInput, setShowVerifyInput] = useState(false)
+
+  const handleShowVerifyInput = () => {
+    setShowVerifyInput(!showVerifyInput)
+  }
+
+  return (
+    <div className="forgot-place">
+      <h1>Forgot</h1>
+      <form className='form-place'>
+        <input
+          className='input-box'
+          type="email"
+          name="userEmail"
+          id="userEmail"
+          placeholder='Email'
+        />
+        {
+          showVerifyInput &&
+          (
+            <>
+              <div className='input-password'>
+                <input
+                  className='input-box'
+                  type={showPassword ? 'text' : 'password'}
+                  name="userPassword"
+                  id="userPassword"
+                  placeholder='Password'
+                />
+                {
+                  showPassword
+                    ? (
+                      <BiHide
+                        className='show-password-icon'
+                        size={25}
+                        onClick={handleShowPasswordButton}
+                      />
+                    )
+                    : (
+                      <BiShow
+                        className='show-password-icon'
+                        size={25}
+                        onClick={handleShowPasswordButton}
+                      />
+                    )
+                }
+              </div>
+              <input
+                className='input-box'
+                type="text"
+                name="verify"
+                id="verify"
+                placeholder='請輸入驗證碼'
+              />
+            </>
+          )
+        }
+        <div className="submit-place">
+          <input
+            className='input-submit'
+            type="button"
+            value={showVerifyInput ? '重新設定密碼' : '驗證'}
+            onClick={handleShowVerifyInput}
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Forgot
--- client/src/pages/User/Login/LoginModal.js
+++ client/src/pages/User/Login/LoginModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './LoginModal.scss'
 import SignupModal from '../Signup/SignupModal'
+import Forgot from './Forgot'
 import { IoCloseSharp } from 'react-icons/io5'
 import { FaFacebookSquare } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
@@ -11,10 +12,19 @@ import { BiShow, BiHide, BiArrowBack } from 'react-icons/bi'
 const LoginModal = ({ handleToggleLoginModal }) => {
   const [signupModal, setSignupModal] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
+  const [userForgot, setUserForgot] = useState(false)
 
   // Signup modal
   const handleToggleSignupModal = () => {
-    setSignupModal(!signupModal)
+    if (userForgot) {
+      setUserForgot(!userForgot)
+    } else {
+      setSignupModal(!signupModal)
+    }
+  }
+  // Forgot page
+  const handleToggleForgotPage = () => {
+    setUserForgot(!userForgot)
   }
 
   const handleCloseLoginModal = (event) => {
@@ -36,20 +46,19 @@ const LoginModal = ({ handleToggleLoginModal }) => {
         <div className="login-modal-content-background">
           <div className="close-login-button">
             {
-              signupModal
+              signupModal || userForgot
                 ? (
-
-                  <button
+                  <div
                     onClick={handleToggleSignupModal}>
-                    <BiArrowBack size={25} />
-                  </button>
+                    <BiArrowBack size={30} />
+                  </div>
                 )
                 : (
 
-                  <button
+                  <div
                     onClick={handleToggleLoginModal}>
-                    <IoCloseSharp size={25} />
-                  </button>
+                    <IoCloseSharp size={30} />
+                  </div>
                 )
             }
           </div>
@@ -65,94 +74,107 @@ const LoginModal = ({ handleToggleLoginModal }) => {
                 />
               )
               : (
-
-                <div className="login-place">
-                  <h1>Login</h1>
-                  <form className='form-place'>
-                    <input
-                      className='input-box'
-                      type="email"
-                      name="userEmail"
-                      id="userEmail"
-                      placeholder='Email'
+                userForgot
+                  ? (
+                    <Forgot
+                      showPassword={showPassword}
+                      handleShowPasswordButton={handleShowPasswordButton}
                     />
-                    <div className='input-password'>
-                      <input
-                        className='input-box'
-                        type={showPassword ? 'text' : 'password'}
-                        name="userPassword"
-                        id="userPassword"
-                        placeholder='Password'
-                      />
-                      {
-                        showPassword
-                          ? (
-                            <BiHide
-                              className='show-password-icon'
-                              size={25}
-                              onClick={handleShowPasswordButton}
-                            />
-                          )
-                          : (
-                            <BiShow
-                              className='show-password-icon'
-                              size={25}
-                              onClick={handleShowPasswordButton}
-                            />
-                          )
-                      }
-                    </div>
-                    <div className="login-option">
-                      <div className="check-box">
+                  )
+                  : (
+                    <div className="login-place">
+                      <h1>Login</h1>
+                      <form className='form-place'>
                         <input
-                          type="checkbox"
-                          name="remember"
-                          id="remember"
+                          className='input-box'
+                          type="email"
+                          name="userEmail"
+                          id="userEmail"
+                          placeholder='Email'
                         />
-                        <label className='text-style' htmlFor='remember'>
-                          記住我
-                        </label>
-                      </div>
-                      <div>
-                        <a
-                          className='text-style'
-                          onClick={handleToggleSignupModal}
-                        >
-                          加入會員？
-                        </a>
-                        <a className='text-style' href="#">忘記密碼？</a>
-                      </div>
+                        <div className='input-password'>
+                          <input
+                            className='input-box'
+                            type={showPassword ? 'text' : 'password'}
+                            name="userPassword"
+                            id="userPassword"
+                            placeholder='Password'
+                          />
+                          {
+                            showPassword
+                              ? (
+                                <BiHide
+                                  className='show-password-icon'
+                                  size={25}
+                                  onClick={handleShowPasswordButton}
+                                />
+                              )
+                              : (
+                                <BiShow
+                                  className='show-password-icon'
+                                  size={25}
+                                  onClick={handleShowPasswordButton}
+                                />
+                              )
+                          }
+                        </div>
+                        <div className="login-option">
+                          <div className="check-box">
+                            <input
+                              type="checkbox"
+                              name="remember"
+                              id="remember"
+                            />
+                            <label className='text-style' htmlFor='remember'>
+                              記住我
+                            </label>
+                          </div>
+                          <div>
+                            <a
+                              className='text-style'
+                              onClick={handleToggleSignupModal}
+                            >
+                              加入會員？
+                            </a>
+                            <a
+                              className='text-style'
+                              onClick={handleToggleForgotPage}
+                            >
+                              忘記密碼？
+                            </a>
+                          </div>
+                        </div>
+                        <div className="submit-place">
+                          <input
+                            className='input-submit'
+                            type="submit"
+                            value="登入"
+                          />
+                          <p className='other-login-text'>其他登入方式</p>
+                          <div className='other-login-place'>
+                            <button
+                              className='other-login-btn btn-google'
+                              type="button"
+                            >
+                              <FcGoogle size={25} />
+                            </button>
+                            <button
+                              className='other-login-btn btn-facebook'
+                              type="button"
+                            >
+                              <FaFacebookSquare color='#fff' size={25} />
+                            </button>
+                            <button
+                              className='other-login-btn btn-apple'
+                              type="button"
+                            >
+                              <AiFillApple color='#fff' size={25} />
+                            </button>
+                          </div>
+                        </div>
+                      </form>
                     </div>
-                    <div className="submit-place">
-                      <input
-                        className='input-submit'
-                        type="submit"
-                        value="登入"
-                      />
-                      <p className='other-login-text'>其他登入方式</p>
-                      <div className='other-login-place'>
-                        <button
-                          className='other-login-btn btn-google'
-                          type="button"
-                        >
-                          <FcGoogle size={25} />
-                        </button>
-                        <button
-                          className='other-login-btn btn-facebook'
-                          type="button"
-                        >
-                          <FaFacebookSquare color='#fff' size={25} />
-                        </button>
-                        <button
-                          className='other-login-btn btn-apple'
-                          type="button"
-                        >
-                          <AiFillApple color='#fff' size={25} />
-                        </button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
+                  )
               )
           }
         </div>

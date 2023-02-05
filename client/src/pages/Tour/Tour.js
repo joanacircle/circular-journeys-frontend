c7cae4e66983dfdf70763997678c8c57341c6799
Anchor2333 <117142464+Anchor2333@users.noreply.github.com>
2023-02-05 10:04:23 +0800

    CreateCard

--- client/src/pages/Tour/Tour.js
+++ client/src/pages/Tour/Tour.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import TourTitle from './components/TourTitle/TourTitle'
-
+import TourCard from './components/TourCard/TourCard'
 const Tour = () => {
   return (
     <>
+    <TourCard />
     <TourTitle />
     </>
   )

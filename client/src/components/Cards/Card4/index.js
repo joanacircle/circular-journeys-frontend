a0bcde83d493d0bc41db769405bafa8e61f04e33
Circle <ja95175@gmail.com>
2023-02-05 13:20:53 +0800

    second version of userblog page

--- client/src/components/Cards/Card4/index.js
+++ client/src/components/Cards/Card4/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { AiOutlineHeart, AiOutlineCalendar } from 'react-icons/ai'
+import { HiOutlineArrowLongRight } from 'react-icons/hi2'
 
 import './Card4.scss'
 
@@ -9,13 +10,8 @@ const Card4 = (props) => {
   return (
     <>
     <div className="card4">
-      <div className="post-header">
-        <h2>{props.title}</h2>
+      <div className="card-header">
         <div className='post-meta'>
-          <div className='post-likes'>
-            <AiOutlineHeart size={40} className='heart-icon'/>
-            <p>{props.likes}</p>
-          </div>
           <div className="post-tags">
             <ul>
               <li># {props.tags}</li>
@@ -23,21 +19,39 @@ const Card4 = (props) => {
               <li># {props.tags}</li>
             </ul>
           </div>
+          <div className='post-likes'>
+            <AiOutlineHeart size={35} className='heart-icon'/>
+          </div>
         </div>
+        <h2>{props.title}</h2>
       </div>
 
-      <div className="img-container">
-        <img src={props.imgSrc} className="card-img" alt={props.imgAlt} />
-      </div>
+      <Link to={props.linkToPost}>
+        <div className="img-container">
+          <img src={props.imgSrc} className="card-img" alt={props.imgAlt} />
+        </div>
+      </Link>
 
-      <div className="post-footer">
-        <div className="create-at-group">
-          <AiOutlineCalendar
-          className='calendar-icon'
-          color='#B7B7B7' size={25} />
-          <p className='create-at'>{props.createAt}</p>
+      <div className="card-footer">
+        <div className="post-meta2">
+          <div className="create-at-group">
+            <AiOutlineCalendar
+            className='calendar-icon'
+            size={25} />
+            <p>{props.createAt}</p>
+          </div>
+          <div className="post-likes-group">
+            <AiOutlineHeart size={25} className='heart-icon'/>
+            <p>{props.likes}</p>
+          </div>
         </div>
         <p className='post-content'>{props.postContent}</p>
+        <Link to={props.linkToPost}>
+          <div className="read-more">
+            <p>閱讀更多</p>
+            <HiOutlineArrowLongRight size={25} className='right-icon'/>
+          </div>
+        </Link>
       </div>
     </div>
     </>
--- client/src/pages/Blog/UserBlog.js
+++ client/src/pages/Blog/UserBlog.js
@@ -9,12 +9,15 @@ import B001 from 'images/Blog/B001.jpeg'
 
 const UserBlog = () => {
   const { memberId } = useParams()
-  // Card4 props:
-  const title = '高雄一日遊-駁二、愛河、瑞豐夜市'
-  const likes = '10k'
-  const tags = '旅遊'
   const memberName = 'CircleChang'
+  // Card4 props:
+  const tags = '旅遊'
+  const title = '高雄一日遊-駁二、愛河、瑞豐夜市'
+  const linkToPost = '#'
+  const imgSrc = B001
+  const imgAlt = 'img'
   const createAt = '2023/02/02'
+  const likes = '10k'
   const postContent = `今天我們先去高雄的駁二藝術特區裡有許多精彩的藝術展覽，我們看了很久才走出來，我們沿著小路漫步，欣賞著各式各樣的藝術作品。接著，我們走到了愛河畔，沿著小徑悠閒地散步，看著河水悠悠地流過。
   最後，我們到了瑞豐夜市，吃了許多美味的小吃。首先，我們品嘗了一道傳統的台灣小吃——臭豆腐。臭豆腐的味道香醇，口感酥脆，而且很有嚼勁。接著，我們點了一道高雄特色的海鮮炒麵，裡面有豐富的海鮮，搭配著Q彈的麵條，吃起來非常美味。最後，我們點了一杯珍珠奶茶，清新的口感搭配著珍珠的Q彈，讓我們在逛夜市的同時又能消暑。整個晚上，我們品嘗了許多美味的小吃，並且在瑞豐夜市的氣氛中度過了一個愉快的夜晚。`
 
@@ -35,7 +38,15 @@ const UserBlog = () => {
                   </Link>
                 </ul>
               </div>
-              <Card4 title={title} likes={likes} tags={tags} imgSrc={B001} createAt= {createAt} postContent={postContent}/>
+              <Card4
+                tags={tags}
+                title={title}
+                linkToPost={linkToPost}
+                imgSrc={imgSrc}
+                imgAlt={imgAlt}
+                createAt= {createAt}
+                likes={likes}
+                postContent={postContent}/>
 
               <div className='userblog-pagination'>
                 <Pagination />

93c598ffb8be0362859a4d77d1dd6b77f5c7770b
chenchiaho <chenchiaho@gmail.com>
2023-02-11 12:56:16 +0800

    working on checkout process ui

--- client/src/components/Stripe/StripeContainer.js
+++ client/src/components/Stripe/StripeContainer.js
@@ -1,15 +1,23 @@
 import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
 import React from "react"
+import CardImage from "./CardImage"
 import PaymentForm from "./PaymentForm"
+
+
 
 const PUBLIC_KEY = 'pk_test_51MVV2XL9vOMbHljwYxDpUN8irGCzLHgUXSxQamceHfPCY7BdMKVaNLkzubYd2kKozQGY5Lh3EVPp6LYcrmVNy4ft00KqpV0vfV'
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
+
 export function StripeContainer() {
+
+
+
   return (
     <Elements stripe={stripeTestPromise}>
+      <CardImage />
       <PaymentForm />
     </Elements>
   )
--- client/src/pages/Checkout/sub-pages/Payment.js
+++ client/src/pages/Checkout/sub-pages/Payment.js
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+
 import { StripeContainer } from 'components/Stripe/StripeContainer'
 import './Payment.scss'
 
 
 const Payment = () => {
 
-  const [showItem, setShowItem] = useState(false)
+  const sampleData = {
+    name: 'Kevin La',
+    address: '56 Starbuck St',
+    city: 'Staten Island',
+    province: 'NY',
+    zip: '10304',
+    country: 'USA',
+    phone: '0917666666'
+  }
 
 
   return (
@@ -13,6 +21,24 @@ const Payment = () => {
       <div className='payment-container'>
         <h5 className='payment-title'>付款資訊</h5>
 
+        <div className="confirm-container">
+          <div className="confirm-info">
+            <div className="confirm-address">
+
+              <div style={{ fontWeight: 'bold' }}>{sampleData.name}</div>
+              <br />
+              {Object.entries(sampleData).map(([key, value], i) => {
+                if (key === "name" || key === "province") return null
+                if (key === "zip") return <div key={key}>{`${sampleData.province} ${sampleData.zip}`}</div>
+                return <div key={key}>{value}</div>
+              })}
+
+            </div>
+            <div className="confirm-products">
+
+            </div>
+          </div>
+        </div>
         <div className='credit-info'>
           <StripeContainer className='stripe-container' />
         </div>

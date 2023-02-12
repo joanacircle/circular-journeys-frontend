54296b943e1ad2876cc7f2aa2207fb7df745c363
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-02-12 23:00:56 +0800

    add users.js

--- server/src/route/user/users.js
+++ server/src/route/user/users.js
@@ -4,21 +4,18 @@ const uuid = require('uuid')
 
 const router = express.Router()
 
-
-//http://localhost:3001/user/userslist
-// router.get('/userslist', (req, res, next) => {
-//   const sql = `SELECT * FROM users_information ORDER BY id ASC`
-//   db.query(sql, (err, result) => {
-//     if (err) throw err
-//     res.json(result)
-//   })
-// })
-
 //http://localhost:3001/user/userslist
 router.get('/userslist', async (req, res, next) => {
   const sql = `SELECT * FROM users_information ORDER BY id ASC`
   const [rows] = await db.query(sql)
   res.json(rows)
+})
+
+//http://localhost:3001/user/login
+router.post('/login', async (req, res, next) => {
+  const { user_email, user_password } = req.body()
+  const sql = `SELECT * FROM users_information WHERE email `
+  
 })
 
 //http://localhost:3002/api/user/signup

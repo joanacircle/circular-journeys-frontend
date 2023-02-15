8de4e3c25640591661c09d459044f763f3a37e9e
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-02-15 23:37:46 +0800

    update databases

--- server/src/databases/users_information.sql
+++ server/src/databases/users_information.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 15, 2023 at 08:25 AM
+-- Generation Time: Feb 15, 2023 at 03:32 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -46,7 +46,7 @@ CREATE TABLE `users_information` (
   `nation` varchar(25) DEFAULT NULL COMMENT '國家',
   `city` varchar(30) DEFAULT NULL COMMENT '城市',
   `districts` varchar(255) DEFAULT NULL COMMENT '區域',
-  `postal_code` varchar(10) DEFAULT NULL COMMENT '郵遞區號',
+  `postalCode` varchar(10) DEFAULT NULL COMMENT '郵遞區號',
   `address` varchar(50) DEFAULT NULL COMMENT '街/道',
   `payment_type` varchar(25) DEFAULT NULL COMMENT '付款種類',
   `provider` varchar(25) DEFAULT NULL COMMENT '供應商',
@@ -58,8 +58,8 @@ CREATE TABLE `users_information` (
 -- Dumping data for table `users_information`
 --
 
-INSERT INTO `users_information` (`id`, `member_id`, `created_at`, `active_status`, `points`, `ticket`, `first_name`, `last_name`, `birthday`, `sex`, `password`, `verify`, `token`, `email`, `telephone`, `nation`, `city`, `districts`, `postal_code`, `address`, `payment_type`, `provider`, `account_no`, `expiry`) VALUES
-(1, '123456', '2022-12-21 02:44:42', 1, 9457, 2, 'Chou', 'Alan', '1995-04-14', 'm', 'e10adc3949ba59abbe56e057f20f883e', 'N6RrotWRS', '3ca06a3b-b40d-4f5c-9a70-ff55cc14585f', 'alanchou19950414@gmail.com', '0970688851', '臺灣', ' 臺中市', '太平區', '80711', '環中東路三段507號6樓之6', '信用卡', 'VISA', '1111 2222 3333 4444', '2022-12-21'),
+INSERT INTO `users_information` (`id`, `member_id`, `created_at`, `active_status`, `points`, `ticket`, `first_name`, `last_name`, `birthday`, `sex`, `password`, `verify`, `token`, `email`, `telephone`, `nation`, `city`, `districts`, `postalCode`, `address`, `payment_type`, `provider`, `account_no`, `expiry`) VALUES
+(1, '123456', '2022-12-21 02:44:42', 1, 9457, 2, 'Chou', 'Alan', '1995-04-14', 'm', 'e10adc3949ba59abbe56e057f20f883e', 'GjG0ecsns', 'dc756407-f3c5-41bd-8828-2f4691c83eac', 'alanchou19950414@gmail.com', '0970688851', '臺灣', ' 臺中市', '太平區', '80711', '環中東路三段507號6樓之6', '信用卡', 'VISA', '1111 2222 3333 4444', '2022-12-21'),
 (2, '123457', '2022-12-21 02:56:42', 1, 8778, 1, 'La', 'Kevin', '0000-00-00', 'f', 'e10adc3949ba59abbe56e057f20f883e', NULL, 'b3778d2a-a206-4982-a551-422658654f8d', 'kevin@gmail.com', '0900000000', '臺灣', '高雄市', NULL, '80711', '高雄市阿公店0號水庫', '信用卡', 'VISA', '1111 2222 3333 4444', '2022-12-21'),
 (4, '123458', '2022-12-21 02:58:19', 1, 8787, 8, '張', '圓圓', '1996-06-05', 'f', 'e10adc3949ba59abbe56e057f20f883e', NULL, '6d24d88a-3fde-41eb-9f65-b537e6e0b8c1', 'circle@gmail.com', '0900000000', '臺灣', '高雄市', '鼓山區', '80711', '巨蛋路', '信用卡', 'VISA', '1111 2222 3333 4444', '2022-12-21'),
 (50, 'ngFnKmtRc', '2023-02-14 17:19:46', 1, 0, 0, '王', '小明', NULL, NULL, 'I07jcgq12', NULL, '7fa641e5-c1f5-48fe-b10e-085296846d35', 'test01@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),

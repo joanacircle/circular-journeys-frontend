0f357cf7b4207ef649767106b0ebcba0a9a37b95
chenchiaho <chenchiaho@gmail.com>
2023-02-16 04:35:26 +0800

    function for product detail page

--- client/src/pages/Shop/ProductDetail.js
+++ client/src/pages/Shop/ProductDetail.js
@@ -1,7 +1,7 @@
 import './data/index'
 import './ProductDetail.scss'
 
-import { useState, useRef } from 'react'
+import { useState, useEffect } from 'react'
 
 const sampleData = [
   {
@@ -9,131 +9,125 @@ const sampleData = [
     name: '白色T-shirt',
     desc: 'description 123',
     price: 100,
-    img: 'https://i.imgur.com/ba3tvGm.jpg'
+    img: [
+      'https://i.imgur.com/ba3tvGm.jpg',
+      'https://i.imgur.com/pHQ3xT3.jpg',
+      'https://i.imgur.com/1GrakTl.jpg',
+      'https://i.imgur.com/ba3tvGm.jpg',
+      'https://i.imgur.com/pHQ3xT3.jpg'
+
+    ]
   },
+
   {
     id: 2,
     name: '黑色T-shirt',
     desc: 'description 123',
     price: 200,
-    img: 'https://i.imgur.com/pHQ3xT3.jpg'
+    img: ['https://i.imgur.com/pHQ3xT3.jpg']
   },
   {
     id: 3,
     name: '咖啡色T-shirt',
     desc: 'description 123',
     price: 300,
-    img: 'https://i.imgur.com/1GrakTl.jpg'
+    img: ['https://i.imgur.com/1GrakTl.jpg']
   },
   {
     id: 4,
     name: '白色T-shirt',
     desc: 'description 123',
     price: 100,
-    img: 'https://i.imgur.com/ba3tvGm.jpg'
+    img: ['https://i.imgur.com/ba3tvGm.jpg']
   },
   {
     id: 5,
     name: '黑色T-shirt',
     desc: 'description 123',
     price: 200,
-    img: 'https://i.imgur.com/pHQ3xT3.jpg'
+    img: ['https://i.imgur.com/pHQ3xT3.jpg']
   },
   {
     id: 6,
     name: '咖啡色T-shirt',
     desc: 'description 123',
     price: 300,
-    img: 'https://i.imgur.com/1GrakTl.jpg'
+    img: ['https://i.imgur.com/1GrakTl.jpg']
   }
 ]
-
-const images =
-  [
-    'https://i.imgur.com/ba3tvGm.jpg',
-    'https://i.imgur.com/pHQ3xT3.jpg',
-    'https://i.imgur.com/1GrakTl.jpg',
-    'https://i.imgur.com/ba3tvGm.jpg',
-    'https://i.imgur.com/pHQ3xT3.jpg',
-    'https://i.imgur.com/1GrakTl.jpg'
-
-  ]
-
 
 
 const ProductDetail = () => {
 
-  const [activeIndex, setActiveIndex] = useState(0)
-  const [activeThumbnailIndex, setActiveThumbnailIndex] = useState(0)
-  const [recommendations, setRecommendations] = useState([])
-  const mainImageContainerRef = useRef(null)
+  const [selectedThumbnailIndex, setSelectedThumbnailIndex] = useState(0)
 
   const handleThumbnailClick = (index) => {
-    setActiveIndex(index)
-    setActiveThumbnailIndex(index)
-    mainImageContainerRef.current.scrollTo({
-      top: index * mainImageContainerRef.current.clientHeight,
+    setSelectedThumbnailIndex(index)
+
+    window.scrollTo({
+      top: index * 150,
       behavior: 'smooth'
     })
   }
 
-  const handleMainImageScroll = () => {
-    const index = Math.round(mainImageContainerRef.current.scrollTop / mainImageContainerRef.current.clientHeight)
-    setActiveIndex(index)
-    setActiveThumbnailIndex(index)
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY
+      const index = Math.floor(scrollPosition / 150)
+      setSelectedThumbnailIndex(index)
+    }
 
-  const addRandomRecommendation = () => {
-    const shuffledData = sampleData.sort(() => 0.5 - Math.random())
-    const selectedData = shuffledData.slice(0, 4)
-    setRecommendations(selectedData)
-  }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
     <>
       <div className='product-detail-page'>
         <div className="product-detail">
           <div className="product-image-gallery">
-
-            <div className="thumbnail-container">
-              {sampleData.map((product, index) => (
-                <div className={`thumbnail ${index === activeThumbnailIndex ? 'active' : ''}`} key={`thumbnail-${index}`} onClick={() => handleThumbnailClick(index)}>
-                  <img src={product.img} alt={`product-thumbnail-${index}`} />
-                </div>
+            <div className="thumbnails-container">
+              {sampleData[0].img.map((pic, index) => (
+                <img
+                  className={`thumbnail ${selectedThumbnailIndex === index ? 'selected' : ''}`}
+                  src={pic}
+                  alt=""
+                  key={index}
+                  onClick={() => handleThumbnailClick(index)}
+                />
               ))}
             </div>
-            <div className="main-image-container" onScroll={handleMainImageScroll} ref={mainImageContainerRef}>
-              {images.map((image, index) => (
-                <img src={image} alt={`product-${index}`} key={`product-${index}`} className={`main-image ${index === activeIndex ? 'active' : ''}`} />
+            <div className="main-image-container" >
+              {sampleData[0].img.map((pic, index) => (
+                <img className='image-list' src={pic} alt="" key={index} />
               ))}
             </div>
             <div className='product-info-container'>
               <div className='product-spec'>
-
-                <div className="product">
-
-                  <h2>{sampleData[0].name}</h2>
-
-                  <p>Price: ${sampleData[0].price}</p>
-                  <p>{sampleData[0].desc}</p>
-                </div>
-
+                <h2>{sampleData[0].name}</h2>
+                <p>Price: ${sampleData[0].price}</p>
+                <p>{sampleData[0].desc}</p>
+              </div>
+              <div className='spec-buttons'>
+                <button className='put-kart'>放入購物包</button>
+                <button className='buy-now'>立即購買</button>
               </div>
             </div>
           </div>
-
         </div>
         <div className='recommendations'>
           <div className='recommend-list'>
-            {recommendations.map((item) => (
+            {sampleData.map((item) => (
               <div key={item.id} className='recommend-product'>
-                <h2>{item.name}</h2>
+                <img className='rec-img' src={item.img[0]} alt="" />
+                <h5>{item.name}</h5>
                 <p>Price: ${item.price}</p>
-                <p>{item.desc}</p>
               </div>
             ))}
           </div>
-          <button onClick={addRandomRecommendation}>Add Recommendations</button>
         </div>
       </div>
     </>

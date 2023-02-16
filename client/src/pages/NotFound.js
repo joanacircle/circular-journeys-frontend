9644e9a7f3b950b845b88258163c37456091072e
Circle <ja95175@gmail.com>
2023-02-17 00:15:08 +0800

    set UI of NotFound page

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom"
+
+import './NotFound.scss'
+
 export const NotFound = () => {
   return (
-    <>404 NotFound</>
+    <>
+    <div className="background">
+      <div className="words">
+        <h3>此頁面放假去了</h3>
+        <h3>您也該放個假了</h3>
+        <h5>讓我們開始吧</h5>
+        <h5>
+          <Link to={'/'}>首頁</Link>
+        </h5>
+      </div>
+    </div>
+    </>
   )
 }

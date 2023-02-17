81547628a9f3359e0cebb9f3b0488ecb783e449b
Circle <ja95175@gmail.com>
2023-02-17 10:45:33 +0800

    add post editor page of unfinished version

---
+++ client/src/pages/Blog/PostEditor.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const PostEditor = () => {
+  return (
+    <>
+    <div className='edit-container'>
+      <div className="edit-wrapper">
+        <form className="edit-form-group">
+          <div className="edit-form-group">
+            <label htmlFor="title">文章標題</label>
+            <input type="text" name='title' placeholder='文章標題...' className='' required/>
+          </div>
+
+          <div className="edit-form-group">
+            <label htmlFor="tag">文章關鍵字</label>
+            <input type="text" name='tag' placeholder='文章關鍵字...' className='' required/>
+          </div>
+
+          <div className="edit-form-group">
+            <label htmlFor="tag">文章關鍵字</label>
+            <textarea name="tag" id="" cols="30" rows="30" placeholder='文章內文...'></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default PostEditor

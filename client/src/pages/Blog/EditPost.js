bd877c2bc9b9d843380d39ad3d46884d82995e5f
Circle <ja95175@gmail.com>
2023-02-23 23:42:38 +0800

    make a edit-post page for modify post

---
+++ client/src/pages/Blog/EditPost.js
@@ -0,0 +1,210 @@
+import { useState, useEffect } from 'react'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import './EditPost.scss'
+
+const EditPost = () => {
+  const { postId } = useParams()
+  const [form, setForm] = useState({
+    postId: `${postId}`,
+    title: '',
+    tags: '',
+    tag1: '',
+    tag2: '',
+    tag3: '',
+    coverPath: '',
+    content: ''
+  })
+  const category = ['美食', '景點', '住宿']
+  const [ctag, setCtag] = useState(
+    category.map((v, i) => {
+      return ({ id: i, value: v, checked: false })
+    })
+  )
+
+  useEffect(() => { getData() }, [])
+  function getData () {
+    axios.get(`${process.env.REACT_APP_DEV_URL}/blog/post/${postId}`)
+    .then(r => {
+      const { post_title, tag, cover, post_content } = r.data[0]
+
+      const tags = Object.values(tag).filter((v) => (
+        category.includes(v)
+      )) // 分類
+      const restTags = Object.values(tag).filter((v) => (
+        !category.includes(v)
+      )) // 剩下的關鍵字
+
+      setForm({
+        postId,
+        title: post_title,
+        tags: tags || '',
+        tag1: restTags[0] || '',
+        tag2: restTags[1] || '',
+        tag3: restTags[2] || '',
+        coverPath: cover,
+        content: post_content
+      })
+
+      const newCtag = ctag.map((v, i) => {
+        if (tags.includes(v.value)) {
+          return ({ ...v, checked: true })
+        } else {
+          return ({ ...v })
+        }
+      })
+      setCtag(newCtag)
+     })
+    .catch(err => console.log(err))
+  }
+
+  // 分類
+  useEffect(() => {
+    const newTags = ctag.filter((v) => v.checked).map((v) => v.value)
+    setForm({ ...form, tags: newTags })
+   }, [ctag])
+
+  // 標題、關鍵字
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  // 傳到後端
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.put(`${process.env.REACT_APP_DEV_URL}/blog/post/${postId}`, form)
+    .then(r => {
+      console.log(r.data)
+      // window.location = `http://localhost:3000/blog/post/${postId}`
+    }
+    )
+    .catch(err => console.log(err))
+  }
+console.log(form)
+
+  return (
+    <>
+    <div className='edit-container'>
+      <div className="edit-wrapper">
+        <h2>修改文章</h2>
+        <form className="edit-form-group"
+          onSubmit={handleSubmit}>
+          <div className="form-item">
+            <label htmlFor="title">文章標題</label>
+            <input
+            type="text" name='title' className=''
+            value={form.title}
+            onChange={handleChange}
+            placeholder={form.title} required/>
+          </div>
+
+          <div className="form-item">
+            <p>文章分類</p>
+            {ctag.map((v, i) => (
+              <div key={i}>
+                <input
+                type='checkbox'
+                value={v.value}
+                checked={v.checked}
+                onChange={() => {
+                  const newCtag = ctag.map((v2, i2) => {
+                    if (v2.id === v.id) return { ...v2, checked: !v2.checked }
+                    return { ...v2 }
+                  })
+                  setCtag(newCtag)
+                }}
+                />
+                <label>{v.value}</label>
+              </div>
+            ))}
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="tag1">文章關鍵字1</label>
+            <input
+            type="text" name='tag1'
+            value={form.tag1}
+            onChange={handleChange}
+            placeholder='#關鍵字'
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="tags">文章關鍵字2</label>
+            <input
+            type="text" name='tag2'
+            value={form.tag2}
+            onChange={handleChange}
+            placeholder='#關鍵字'
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="tag3">文章關鍵字3</label>
+            <input
+            type="text" name='tag3'
+            value={form.tag3}
+            onChange={handleChange}
+            placeholder='#關鍵字'
+            />
+          </div>
+
+          {/* 問題：有時候頁面重整會得不到cover的資料 */}
+          <div className="form-item">
+            <label>文章首圖</label>
+            <CKEditor
+              editor={ ClassicEditor }
+              data={form.coverPath}
+              onChange={(event, editor) => {
+                const data = editor.getData()
+                setForm({ ...form, coverPath: data })
+              }}
+              config={
+                {
+                  ckfinder: {
+                    uploadUrl: `${process.env.REACT_APP_DEV_URL}/blog/upload-cover`
+                  },
+                  toolbar: ['imageUpload']
+                }
+              }
+            />
+          </div>
+
+          <div className="form-item">
+            <label>文章內文</label>
+            <CKEditor
+              editor={ ClassicEditor }
+              data={form.content}
+              onChange={ (event, editor) => {
+                const data = editor.getData()
+                setForm({ ...form, content: data })
+              } }
+              config={
+                {
+                  ckfinder: {
+                    uploadUrl: `${process.env.REACT_APP_DEV_URL}/blog/upload-img`
+                  }
+                }
+              }
+            />
+          </div>
+
+          <div className="form-item">
+            <input
+            type="submit"
+            name='submit'
+            value='完成'
+            className='submit-btn'/>
+          </div>
+        </form>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default EditPost
+
--- server/src/route/blog/blog.js
+++ server/src/route/blog/blog.js
@@ -98,7 +98,7 @@ router.post('/upload-img', MultipartyMiddleWare, (req, res) => {
   }
 })
 
-// http://localhost:3001/newpost/:member_id // PostEditor
+// http://localhost:3001/blog/newpost/:member_id // PostEditor
 router.post('/newpost/:member_id', async(req, res) => {
   const { memberId, title, tags, tag1, tag2, tag3, cover, content } = req.body
   const postId = 'p' + uuid.v4()
@@ -136,64 +136,33 @@ router.post('/newpost/:member_id', async(req, res) => {
     })
   }
   catch(err){
-    res.send(err)
+    res.json({message: err})
   }
 })
 
-// http://localhost:3001/blog/:member_id // UserBlog
-router.get('/:member_id', async (req, res) => {
-  const member_id = req.params.member_id;
+// http://localhost:3001/blog/postdata/:postId // EditPost
+router.delete('/post/:postId', async(req, res)=>{
 
-  const sql =`
-  SELECT 
-    posts.post_id,  
-    posts.create_at,  
-    posts.post_title, 
-    posts.post_content, 
-    posts.total_likes,
-    posts.cover, 
-    users_information.user_nickname,
-    (
-      SELECT JSON_OBJECTAGG(post_tags.tag_id, post_tags.tag)
-      FROM post_tags 
-      WHERE post_tags.post_id = posts.post_id
-    ) 
-    AS tag 
-  FROM users_information 
-  JOIN posts 
-  ON users_information.member_id = ?
-  WHERE posts.member_id = ?
-  ORDER BY create_at DESC
-  `
-  const [rows] = await db.query(sql, [member_id, member_id],
-    (err, result) => {
-      if (err) throw err
-      res.json(result)
-    });
-
-  rows.forEach(row => {
-    row.create_at = moment(row.create_at).format('YYYY/MM/DD');
-  });
-  res.json(rows);
 })
 
-// http://localhost:3001/blog/post/:post_id // SinglePost
+// http://localhost:3001/blog/post/:post_id // SinglePost, EditPost
 router.get('/post/:post_id', async (req, res) => {
   const post_id = req.params.post_id;
   const sql =`
   SELECT 
-    posts.post_title, 
-    posts.member_id,
-    users_information.user_nickname,
-    posts.create_at,  
-    posts.total_likes,
-    posts.post_content, 
-    posts.post_id, 
-    posts.cover, 
-    (
-      SELECT JSON_OBJECTAGG(post_tags.tag_id, post_tags.tag)
-      FROM post_tags 
-      WHERE post_tags.post_id = posts.post_id
+  posts.post_id,
+  posts.post_title, 
+  posts.member_id,
+  users_information.user_nickname,
+  posts.create_at,  
+  posts.total_likes,
+  posts.post_content, 
+  posts.post_id, 
+  posts.cover, 
+  (
+    SELECT JSON_OBJECTAGG(post_tags.tag_id, post_tags.tag)
+    FROM post_tags 
+    WHERE post_tags.post_id = posts.post_id
     ) 
     AS tag
   FROM users_information 
@@ -201,17 +170,96 @@ router.get('/post/:post_id', async (req, res) => {
   ON posts.member_id = users_information.member_id 
   WHERE posts.post_id = ?
   `
-
+    
   const [rows] = await db.query(sql, [post_id],
     (err, result) => {
       if (err) throw err
       res.json(result)
     });
-
-  rows.forEach(row => {
-    row.create_at = moment(row.create_at).format('YYYY/MM/DD');
-  });
-  res.json(rows);
+    
+    rows.forEach(row => {
+      row.create_at = moment(row.create_at).format('YYYY/MM/DD');
+      if(row.tag === null){
+        row.tag = []
+      }
+    });
+    res.json(rows);
 })
+
+// http://localhost:3001/blog/post/:postId // EditPost
+router.put('/post/:post_id', async(req, res)=>{
+  const postId = req.params
+  const {title, tags, tag1, tag2, tag3, cover, content} = req.body
+  const totalTag = [tag1, tag2, tag3].filter((v)=>{
+    return (v.length>0)
+  })
+  if(tags.length>0){
+    tags.map((v, i) => {totalTag.push(v)})
+  }
+
+  const sqlUpdatePost = `
+  UPDATE posts SET post_id=?,modify_at=now(), post_title=?,post_content=?,cover=? WHERE 1`
+  const sqlSelectTag=`
+  SELECT tag_id FROM post_tags WHERE tag = ? LIMIT 1`
+  const sqlUpdateTag=`
+  UPDATE post_tags SET tag_id=?, tag=? WHERE post_id=?`
+  
+  try{
+    const [result1] = await db.query(sqlUpdatePost, [postId, title, content, cover])
+
+    for(const tag of totalTag){
+      const [result2] = await db.query(sqlSelectTag, [tag])
+      if(result2.length>0){
+        const exisitTagId = result2[0].tag_id
+        const [result2] = await db.query(sqlUpdateTag, [exisitTagId, tag, postId])
+      }else{
+        const tagId = 't'+uuid.v4()
+        const [result3] = await db.query(sqlUpdateTag, [tagId, tag, postId])
+      }
+    }
+    res.json({message: 'success'})
+  }
+  catch(err){
+    res.send(err)
+  }
+})
+
+// http://localhost:3001/blog/:member_id // UserBlog
+router.get('/:member_id', async (req, res) => {
+const member_id = req.params.member_id;
+
+const sql =`
+SELECT 
+  posts.post_id,  
+  posts.create_at,  
+  posts.post_title, 
+  posts.post_content, 
+  posts.total_likes,
+  posts.cover, 
+  users_information.user_nickname,
+  (
+    SELECT JSON_OBJECTAGG(post_tags.tag_id, post_tags.tag)
+    FROM post_tags 
+    WHERE post_tags.post_id = posts.post_id
+  ) 
+  AS tag 
+FROM users_information 
+JOIN posts 
+ON users_information.member_id = ?
+WHERE posts.member_id = ?
+ORDER BY create_at DESC
+`
+const [rows] = await db.query(sql, [member_id, member_id],
+  (err, result) => {
+    if (err) throw err
+    res.json(result)
+  });
+
+rows.forEach(row => {
+  row.create_at = moment(row.create_at).format('YYYY/MM/DD');
+});
+res.json(rows);
+})
+
 
 module.exports = router;

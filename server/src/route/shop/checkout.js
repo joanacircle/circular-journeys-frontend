6dc1daf17ce8a247764fe539625987ff5160660c
chenchiaho <chenchiaho@gmail.com>
2023-02-25 23:45:20 +0800

    implement query and delete of shipping address

--- server/src/route/shop/checkout.js
+++ server/src/route/shop/checkout.js
@@ -3,11 +3,31 @@ const router = express.Router()
 const db = require('../../model/connect-sql')
 
 router.get('/', async (req, res) => {
-  const sql = "SELECT * FROM `users_information`"
+  // const { member_id } = req.query
+  const member_id = '104709174078800080046'
+  const sql = `SELECT * FROM user_address WHERE member_id = '${member_id}'`
   const [data, index] = await db.query(sql)
   res.json(data)
 })
 
-router.get
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  const { member_id } = req.body
+  // console.log(`Requesting values are: ${id} and ${member_id}`)
+  try {
+
+    const sql = `DELETE FROM user_address WHERE id = ? AND member_id = ?`
+
+    const [result, index] = await db.query(sql, [id, member_id])
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: 'Address not found' })
+    }
+    res.json({ message: 'address deleted' })
+  } catch (error) {
+    console.log(`Error deleting address: ${error}`)
+    res.status(500).json({ message: 'Server error' })
+  }
+})
 
 module.exports = router

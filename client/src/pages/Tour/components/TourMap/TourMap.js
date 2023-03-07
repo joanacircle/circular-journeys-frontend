8c4e7ba56410e7bb49c75b0a2c8b341a159388cd
Anchor Hsiao <jim383895a@gmail.com>
2023-03-07 18:43:09 +0800

    edit data

--- client/src/pages/Tour/components/TourMap/TourMap.js
+++ client/src/pages/Tour/components/TourMap/TourMap.js
@@ -4,8 +4,6 @@ import L from 'leaflet'
 import "leaflet/dist/leaflet.css"
 import placeholder from "../../../../images/Tour/placeholder.png"
 
-// import { response } from 'express'
-import { data } from 'pages/Tour/data'
 
 
 
@@ -15,11 +13,16 @@ const icon = L.icon({
   iconAnchor: [19, 38]
 })
 export default function TourMap() {
+  // 地圖參考點 標記參考點
   const mapRef = useRef(null)
   const markerRef = useRef()
+  // 起始位置(Lat, Lng)經緯度 與預設地點
   const [position, setPosition] = useState([22.63940, 120.30256])
+  const [address, setaddress] = useState(['台灣高雄市蓮池潭'])
+  // 透過Nominatim 搜尋關鍵字 得到經緯度
   useEffect(() => {
-    const address = '高雄市蓮池潭'
+    console.log('24', address)
+
     const MapUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=jsonv2`
     fetch(MapUrl)
     .then(response => response.json())
@@ -28,16 +31,12 @@ export default function TourMap() {
       if (data && data.length > 0) {
         const { lat, lon } = data[0]
         setPosition([lat, lon])
-        markerRef.current.setLatLng([lat, lon])
-        mapRef.current.setView([lat, lon], 13)
-        mapRef.current.flyTo([lat, lon], 18)
+        mapRef.current.flyTo([lat, lon], 14, { animate: true })
+        console.log('31', mapRef)
       }
     })
   }, [])
 
-  // useEffect(() => {
-  //   markerRef.current.openPopup()
-  // }, [position])
   return (
 
   <MapContainer
@@ -45,15 +44,16 @@ export default function TourMap() {
    zoom={13}
    style={{ width: '100%', height: "100%" }}
    scrollWheelZoom={false}
+   ref={mapRef}
    >
     <TileLayer
-      attribution='&copy; OpenStreetMap contributors'
+      attribution='&copy; OpenStreetMap contributors '
       url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      // TODO:下面網址會切換地圖顯示模式，會比較好看但是次數有限制，所以展示才使用(有100次可以用)。
+      // url='https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=D3Te9vzolvZQczBKWWL2'
     />
-    <Marker position={position} icon={icon}>
+    <Marker position={position} icon={icon} ref={markerRef}>
       <Popup>
-        A pretty CSS3 popup. <br />
-        Easily customizable.
       </Popup>
     </Marker>
   </MapContainer>

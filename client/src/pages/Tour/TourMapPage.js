c744ab0a7898d741a9dee0a0fb34b66927e8ecdf
Anchor Hsiao <jim383895a@gmail.com>
2023-03-08 16:40:41 +0800

    add click eventListener  change marker position

--- client/src/pages/Tour/TourMapPage.js
+++ client/src/pages/Tour/TourMapPage.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TourMap from './components/TourMap/TourMap'
 import TourSearchBar from './components/TourSearchBar/TourSearchBar'
+import TourCardList from './components/TourCardList/TourCardList'
+import { data as TourCards } from './data/index'
 
 export default function TourMapPage() {
+
+  const [selectedCard, setselectedCard] = useState(null)
+
+ function clickEvent(card) {
+  console.log('11', card.name)
+  console.log('12', card.adress)
+  setselectedCard(`${card.adress}${card.name}`)
+  console.log('14', selectedCard)
+  }
+
   return (
     <>
     <div>
       <h1>
       TourMapPage
       </h1>
-      <div style={{ display: "flex" }}>
-        <div style={{ border: "2px solid blue", width: "700px", height: "560px", zIndex: 9 }}>
-          <TourMap />
-        </div>
-        <div style={{ border: "2px solid blue", width: "700px", height: "560px" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ border: "2px solid blue" }}>
           <TourSearchBar />
+          <div>
+          <TourCardList cards={TourCards} onClickCard={clickEvent}/>
+          </div>
+        </div>
+        <div style={{ border: "2px solid blue", width: "60vw", height: "380px", zIndex: 9 }}>
+          <TourMap selectedCard={selectedCard}/>
         </div>
       </div>
     </div>
--- client/src/pages/Tour/components/TourMap/TourMap.js
+++ client/src/pages/Tour/components/TourMap/TourMap.js
@@ -12,50 +12,49 @@ const icon = L.icon({
   iconSize: [38, 38],
   iconAnchor: [19, 38]
 })
-export default function TourMap() {
+export default function TourMap({ selectedCard }) {
   // 地圖參考點 標記參考點
   const mapRef = useRef(null)
   const markerRef = useRef()
   // 起始位置(Lat, Lng)經緯度 與預設地點
   const [position, setPosition] = useState([22.63940, 120.30256])
-  const [address, setaddress] = useState(['台灣高雄市蓮池潭'])
+  const [address, setaddress] = useState([null])
   // 透過Nominatim 搜尋關鍵字 得到經緯度
   useEffect(() => {
-    console.log('24', address)
-
-    const MapUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=jsonv2`
-    fetch(MapUrl)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      if (data && data.length > 0) {
-        const { lat, lon } = data[0]
-        setPosition([lat, lon])
-        mapRef.current.flyTo([lat, lon], 14, { animate: true })
-        console.log('31', mapRef)
-      }
-    })
-  }, [])
+    if (selectedCard) {
+      const MapUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(selectedCard)}&format=jsonv2`
+      fetch(MapUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.length > 0) {
+          const { lat, lon } = data[0]
+          setPosition([lat, lon])
+          mapRef.current.flyTo([lat, lon], 14, { animate: true })
+        }
+      })
+    }
+  }, [selectedCard])
 
   return (
-
-  <MapContainer
-   center={position}
-   zoom={13}
-   style={{ width: '100%', height: "100%" }}
-   scrollWheelZoom={false}
-   ref={mapRef}
-   >
+  <>
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ width: '100%', height: "100%" }}
+      scrollWheelZoom={false}
+      ref={mapRef}
+    >
     <TileLayer
       attribution='&copy; OpenStreetMap contributors '
       url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       // TODO:下面網址會切換地圖顯示模式，會比較好看但是次數有限制，所以展示才使用(有100次可以用)。
       // url='https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=D3Te9vzolvZQczBKWWL2'
-    />
-    <Marker position={position} icon={icon} ref={markerRef}>
-      <Popup>
-      </Popup>
-    </Marker>
-  </MapContainer>
+      />
+      <Marker position={position} icon={icon} ref={markerRef}>
+        <Popup>
+        </Popup>
+      </Marker>
+    </MapContainer>
+  </>
 )
 }

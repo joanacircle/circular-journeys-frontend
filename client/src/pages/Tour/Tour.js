b4bd7e85e09cb76eac00f45b0ad0b4c230e001fa
Anchor2333 <117142464+Anchor2333@users.noreply.github.com>
2023-03-13 01:02:18 +0800

    edit css tour Js

--- client/src/pages/Tour/Tour.js
+++ client/src/pages/Tour/Tour.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TourBanner from './components/TourBanner/TourBanner'
-import TourCardList from './components/TourOverview/TourOverview'
+import TourOverview from './components/TourOverview/TourOverview'
 import './Tour.scss'
 import { Link } from 'react-router-dom'
 
@@ -13,7 +13,7 @@ const Tour = () => {
       <Link to={'/tour/tourmappage'}>
         GOGO
       </Link>
-        <TourCardList />
+        <TourOverview/>
       </div>
     </>
   )
--- client/src/pages/Tour/components/TourOverview/TourOverview.js
+++ client/src/pages/Tour/components/TourOverview/TourOverview.js
@@ -1,59 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import TourCardList from '../TourCardList/TourCardList'
-import TourCardList2 from '../TourCardList2/TourCardList2'
-import TourCardList3 from '../TourCardList3/TourCardList3'
-import TourFavoriteList2 from '../TourFavoriteList/TourFavoriteList2'
 import TourFavoriteList from '../TourFavoriteList/TourFavoriteList'
 import { data as TourCards } from '../../data/index'
 import './TourOverview.scss'
 
 const App = (p) => {
   const tourTopCards = TourCards
-  const [list, setlist] = useState([])
-  function handleOnDrop(e) {
-    e.preventDefault()
-    console.log(e)
-    console.log(e.dataTransfer.getData("card"))
-    const dragCards = JSON.parse(e.dataTransfer.getData("card"))
-
-    console.log(dragCards)
-    setlist([...list, dragCards])
-    console.log([...list, dragCards])
-  }
-  function handleDragOver(e) {
-    e.preventDefault()
-  }
   return (
     <>
-  <div className='tour-main-list' onDrop={handleOnDrop} onDragOver={handleDragOver}>
-  <from>
-    <h1>托放景點</h1>
-    {
-          <TourCardList key={list.id} cards={list} />
-    }
-    </from>
-  </div>
   <div className='tour-main-list'>
     <from>
-    <TourCardList2 cards={tourTopCards} />
+    <TourCardList listType='tourCardList2' cardType='TourCard2' cards={tourTopCards} />
     </from>
   </div>
   <div className='tour-main-list'>
     <h1>熱門景點</h1>
     <from>
-    <TourCardList cards={tourTopCards} />
+    <TourCardList listType='tourCardList' cardType='TourCard' cards={tourTopCards} />
     </from>
   </div>
   <div className='tour-main-list'>
     <h1>探索美食地圖</h1>
     <from>
-    <TourCardList3 cards={tourTopCards} />
+    <TourCardList listType='tourCardList3' cardType='TourCard3' cards={tourTopCards} />
     </from>
   </div>
   <div className='tour-main-list'>
     <h1>收藏景點</h1>
     <from>
-    <TourFavoriteList2 cards={tourTopCards} />
+    <TourFavoriteList cardType='TourCard' cards={tourTopCards} />
     </from>
   </div>
   </>

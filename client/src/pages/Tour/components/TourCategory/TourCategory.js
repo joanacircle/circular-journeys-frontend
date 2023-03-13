e07e2be6192a91f2d83e9add4b55acaa1ae15ae9
Anchor Hsiao <jim383895a@gmail.com>
2023-03-13 16:37:06 +0800

    edit css

--- client/src/pages/Tour/components/TourCategory/TourCategory.js
+++ client/src/pages/Tour/components/TourCategory/TourCategory.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TourCategory.scss'
 
 
 
 const TourCategory = ({ category, onClick }) => {
+
+  const [clicked, setClicked] = useState()
+  function handleClick () {
+    setClicked(!clicked)
+    onClick()
+  }
   const { name, context, img } = category
     return (
     <>
-    <div className='tourCategory' onClick={onClick}>
+    <div className={`tourCategory${clicked ? 'Clicked' : ''}`} onClick={handleClick}>
     <img src={require(`images/Tour/${img}.jpg`)}
     className='tourCategoryImg' alt='TourCardImg'/>
       <span className='tourCategoryText'>

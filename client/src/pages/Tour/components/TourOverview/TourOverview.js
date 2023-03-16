e3860e93d8c822ede700da1942bcdd8ae984a8f6
Anchor2333 <117142464+Anchor2333@users.noreply.github.com>
2023-03-17 05:45:49 +0800

    edit data

--- client/src/pages/Tour/components/TourOverview/TourOverview.js
+++ client/src/pages/Tour/components/TourOverview/TourOverview.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import TourCardList from '../TourCardList/TourCardList'
 import TourCategoryList from '../TourCategory/TourCategoryList'
 import { data as TourCards } from '../../data/index'
+import { collectionData } from '../../data/collection'
 import './TourOverview.scss'
-import { Link } from 'react-router-dom'
 const categories = [
   { id: "TC001", name: "景點", context: "", img: "T01", tags: "景點" },
   { id: "TC002", name: "美食", context: "", img: "T02", tags: "美食" },
@@ -37,15 +37,35 @@ const TourOverview = () => {
           selectedCategory={selectedCategory}
         />
       </div>
-      <Link to={'/tour/tourmappage'}>
-        開始規劃行程
-      </Link>
+
+      <div className='tour-main-list'>
+        <h1>熱門行程</h1>
+        <h6>收藏最熱門的旅遊行程吧!</h6>
+        <TourCardList listType='tourCardList' cardType='TourCard' cards={collectionData} />
+      </div>
       <div className='tour-main-list'>
         <h1>熱門景點</h1>
-        <TourCardList listType='tourCardList' cardType='TourCard' cards={filteredCards} />
+        <h6>快來收藏景點安排行程吧!</h6>
+        <TourCardList listType='tourCardList3' cardType='TourCard3' cards={filteredCards} />
       </div>
       <div className='tour-main-list'>
         <h1>探索美食地圖</h1>
+        <h6>製作專屬你的美食地圖!</h6>
+        <TourCardList listType='tourCardList3' cardType='TourCard3' cards={filteredCards} />
+      </div>
+      <div className='tour-main-list'>
+        <h1>住宿</h1>
+        <h6>快來確認距離最近的旅店吧!</h6>
+        <TourCardList listType='tourCardList3' cardType='TourCard3' cards={filteredCards} />
+      </div>
+      <div className='tour-main-list'>
+        <h1>特色文化</h1>
+        <h6>趕快加入體驗在地文化特色!</h6>
+        <TourCardList listType='tourCardList3' cardType='TourCard3' cards={filteredCards} />
+      </div>
+      <div className='tour-main-list'>
+        <h1>自然景觀</h1>
+        <h6>在大自然中好好放鬆!</h6>
         <TourCardList listType='tourCardList3' cardType='TourCard3' cards={filteredCards} />
       </div>
     </>
--- client/src/pages/Tour/data/index.js
+++ client/src/pages/Tour/data/index.js
@@ -5,11 +5,10 @@ const data = [
     { id: "T004", name: "蓮池潭", adress: "高雄市左營區", img: "T04", tags: "景點" },
     { id: "T005", name: "美麗島", adress: "高雄市新興區", img: "T05", tags: "景點" },
     { id: "T006", name: "高雄展覽館", adress: "高雄市前鎮區", img: "T06", tags: "景點" },
-    { id: "T007", name: "蓮池潭", adress: "高雄市左營區", img: "T07", tags: "美食" },
-    { id: "T008", name: "蓮池潭", adress: "高雄市左營區", img: "T08", tags: "美食" },
-    { id: "T009", name: "蓮池潭", adress: "高雄市左營區", img: "T09", tags: "美食" },
-    { id: "T010", name: "蓮池潭", adress: "高雄市左營區", img: "T10", tags: "自然" },
-    { id: "T011", name: "蓮池潭", adress: "高雄市左營區", img: "T11", tags: "文化" },
-    { id: "T012", name: "蓮池潭", adress: "高雄市左營區", img: "T12", tags: "住宿" }
+    { id: "T007", name: "愛河", adress: "高雄市區", img: "T07", tags: "景點" },
+    { id: "T008", name: "85大樓", adress: "高雄市苓雅區", img: "T08", tags: "景點" },
+    { id: "T009", name: "衛武營藝術文化中心", adress: "高雄市鳳山區", img: "T09", tags: "景點" },
+    { id: "T011", name: "流行音樂中心", adress: "高雄市鹽埕區", img: "T11", tags: "景點" },
+    { id: "T012", name: "大港橋", adress: "高雄市鹽埕區", img: "T12", tags: "景點" }
 ]
 export { data }
